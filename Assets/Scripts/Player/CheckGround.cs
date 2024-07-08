9869a7588892c278d5b190ff4da9bc1773198072
duc <ttranhuynhduc.vn@gmail.com>
2024-07-09 01:09:21 +0700

    add Assets Game and add Controller Player

---
+++ Assets/Scripts/Player/CheckGround.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public static bool isGround = true;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isGround = false;
+    }
+}
---
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D rb;
+    [SerializeField]private Animator anim;
+    public float forceJump;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Running();
+    }
+
+    private void Running()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        if (input != 0)
+        {
+            transform.Translate( new Vector2(speed*input*Time.deltaTime,0f));
+            Flip(input);
+            if (!CheckGround.isGround)
+            {
+                anim.SetBool("Jump",true);
+                anim.SetBool("Run",false);
+            }
+            else
+            {
+                anim.SetBool("Run",true);
+            }
+        }
+        else
+        {
+            anim.SetBool("Run",false);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && CheckGround.isGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,forceJump);
+        }
+        if (!CheckGround.isGround)
+        {
+            speed = 2f;
+            anim.SetBool("Jump",true);
+        }
+
+        if (CheckGround.isGround)
+        {
+            speed = 5f;
+            anim.SetBool("Jump",false);
+        }
+    }
+
+    private void Flip(float toward)
+    {
+        float x = transform.localScale.x;
+        if ((toward > 0 && x < 0) || (toward < 0 && x > 0))
+        {
+            transform.localScale = new Vector2(-x, transform.localScale.y);
+        }
+    }
+}
